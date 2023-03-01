6e8fbd7ba5af2353500ab9f28bf767e6e39a4f69
Maksim <dvmax1984@mail.ru>
2023-03-01 16:49:54 +0700

    Решили задачу TASK 002

--- task_002/Program.cs
+++ task_002/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine(" По двум заданным числам проверять является ли одно квадратом другого");
+
+int A = 88;
+int B = 7;
+
+if (A / B == B)
+{
+    Console.WriteLine("Является");
+}
+else
+{
+    Console.WriteLine("Не является");
+}
