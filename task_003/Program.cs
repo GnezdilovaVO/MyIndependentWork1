c9c52af5af12d43b107b8d250059f7a7b8b1e0b5
Maksim <dvmax1984@mail.ru>
2023-03-04 15:52:22 +0700

    Решили задачу TASK003 методами

--- task_003/Program.cs
+++ task_003/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Подсчитать сумму цифр в числе");
+
+static int GetNumber(string s)
+{
+    Console.Write(s);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+static int GetSum(int num)
+{
+    int sum = 0;
+
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+
+    return sum;
+}
+
+
+int num = GetNumber("Введите число: ");
+int sum = GetSum(num);
+
+Console.WriteLine("Сумма числе в числе: " + sum);
+
+
+
+
+
