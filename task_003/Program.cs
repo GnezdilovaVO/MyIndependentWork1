f1e98c6ca91df0f05df53916cdd6541742153a6b
Maksim <dvmax1984@mail.ru>
2023-03-04 16:01:27 +0700

    Решили задачу TASK003 с библиотекой

--- task_003/Program.cs
+++ task_003/Program.cs
@@ -1,23 +1,6 @@
-﻿Console.WriteLine("Подсчитать сумму цифр в числе");
+﻿using static Library;
 
-static int GetNumber(string s)
-{
-    Console.Write(s);
-    return Convert.ToInt32(Console.ReadLine());
-}
-
-static int GetSum(int num)
-{
-    int sum = 0;
-
-    while (num > 0)
-    {
-        sum = sum + num % 10;
-        num = num / 10;
-    }
-
-    return sum;
-}
+Console.WriteLine("Подсчитать сумму цифр в числе");
 
 
 int num = GetNumber("Введите число: ");
