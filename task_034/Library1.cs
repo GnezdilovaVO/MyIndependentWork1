6855f6e2dddcf9c4c0c2d3a8b778c44b1e5a6f85
Valentina <valyapomortseva@gmail.com>
2023-03-08 15:42:34 +0700

    Создала метод по принту массива

---
+++ task_034/Library1.cs
@@ -0,0 +1,16 @@
+public static class Library1
+{
+    public static string printArray(int[] collection)
+    {
+        string massiv = String.Empty;
+        for(int i = 0; i < collection.Length; i++)
+        {
+            if (i == collection.Length-1)
+            {
+            massiv = massiv + collection[i];
+            }
+            massiv = massiv + collection[i] + ",";
+        }
+    }
+    return massiv
+}
